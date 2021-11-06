8fadd0f33c02b47081333ab6ba67b2ffcd9b725e
pratpongwork <pratpong.work@gmail.com>
2021-11-06 13:59:30 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,27 +13,43 @@ class Myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "My app",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello"),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              Text("สวัสดี"),
-              Text("เราชื่อ มิกส์"),
-              Text(
-                "สวัสดี",
-                style: TextStyle(
-                  fontSize: 25,
-                  color: Colors.blue,
-                ),
+      home: MyHomePage(),
+      theme: ThemeData(primarySwatch: Colors.pink),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              "สวัสดี",
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.blue,
               ),
-            ],
-          ),
+            ),
+            Image(
+              image: NetworkImage(
+                  "https://cdn.pixabay.com/photo/2021/10/19/10/56/cat-6723256__340.jpg"),
+            )
+          ],
         ),
       ),
-      theme: ThemeData(primarySwatch: Colors.pink),
     );
   }
 }
